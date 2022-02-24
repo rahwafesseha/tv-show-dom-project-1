6dbf8f8f3bf41a57eea3bd423cbf6295b23ba49f
Rahwa <rahwafesseha@gmail.com>
2022-02-24 19:44:38 +0000

    mandatory completed

--- script.js
+++ script.js
@@ -1,12 +1,59 @@
 //You can edit ALL of the code here
+//edit 
 function setup() {
   const allEpisodes = getAllEpisodes();
+const input = document.getElementById("searchInput");
+input.addEventListener("keyup", () => {
+  const filteredEpisodes = allEpisodes.filter(
+    (epi) =>
+      epi.name.toLowerCase().includes(input.value.toLowerCase()) ||
+      epi.summary.toLowerCase().includes(input.value.toLowerCase())
+  );
+
+  makePageForEpisodes(filteredEpisodes);
+});
+
+  
   makePageForEpisodes(allEpisodes);
 }
 
+function formatNumber(number) {
+  if (number < 10) {
+    return `0${number}`;
+  } else {
+    return `${number}`;
+  }
+}
+ 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  const footer = document.createElement("footer");
+  const source = document.createElement("p");
+  rootElem.innerHTML = "";
+ 
+  episodeList.forEach((episode) => {
+    let episodeContainer = document.createElement("div");
+    episodeContainer.classList.add("mainDiv");
+    let title = document.createElement("h3");
+    title.id = "title";
+    title.classList.add("titleEpisode");
+    title.innerHTML = `${episode.name} - S${formatNumber(
+      episode.season
+    )}E${formatNumber(episode.number)}`;
+    let episodeImage = document.createElement("img");
+    episodeImage.classList.add("imageEpisode");
+    episodeImage.src = episode.image.medium;
+
+    episodeContainer.appendChild(title);
+
+    episodeContainer.appendChild(episodeImage);
+    episodeContainer.innerHTML += episode.summary;
+    rootElem.appendChild(episodeContainer);
+  
+    
+  });
+  
 }
+
 
 window.onload = setup;
