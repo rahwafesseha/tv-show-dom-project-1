41fa9b72c2a40831987fce673ad4fa3e471a7ae9
Rahwa <rahwafesseha@gmail.com>
2022-02-25 19:59:30 +0000

    level 200 and 300 completed

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 //You can edit ALL of the code here
 //edit 
+const filterEpisodesById = (id)=>{
+return allEpisodes.filter((episode)=>
+episode.id.toString() === id
+)};
+
+const searchFilteredEpisodes = (input) => { 
+  return allEpisodes.filter(
+  (epi) =>
+    epi.name.toLowerCase().includes(input.toLowerCase()) ||
+    epi.summary.toLowerCase().includes(input.toLowerCase())
+);
+}
+
+const allEpisodes = getAllEpisodes();
 function setup() {
-  const allEpisodes = getAllEpisodes();
+ 
 const input = document.getElementById("searchInput");
 input.addEventListener("keyup", () => {
-  const filteredEpisodes = allEpisodes.filter(
-    (epi) =>
-      epi.name.toLowerCase().includes(input.value.toLowerCase()) ||
-      epi.summary.toLowerCase().includes(input.value.toLowerCase())
-  );
+  
+  makePageForEpisodes(searchFilteredEpisodes(input.value));
 
-  makePageForEpisodes(filteredEpisodes);
 });
 
   
@@ -24,11 +34,26 @@ function formatNumber(number) {
     return `${number}`;
   }
 }
- 
+ const selectEpisode = document.getElementById("selectEpisode");
+
+ allEpisodes.forEach((episode,index) =>{
+    const selectOptions = document.createElement("option");
+    selectOptions.value = episode.id;
+    selectOptions.innerHTML = `S${formatNumber(episode.season)}E${formatNumber(episode.number)} - ${episode.name}`;
+    selectEpisode.add(selectOptions);
+ })
+selectEpisode.addEventListener("change",(e)=>{
+let selectedEpisodes = filterEpisodesById(e.target.value)
+makePageForEpisodes(selectedEpisodes);
+})
+
+function displayNumber(allEpisodes, filteredEpisodes) {
+  let currentEpisodes = document.getElementById("displayEpisodeNumber");
+  currentEpisodes.innerHTML = `Displaying ${filteredEpisodes}/${allEpisodes}`;
+
+}
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  const footer = document.createElement("footer");
-  const source = document.createElement("p");
   rootElem.innerHTML = "";
  
   episodeList.forEach((episode) => {
@@ -37,19 +62,21 @@ function makePageForEpisodes(episodeList) {
     let title = document.createElement("h3");
     title.id = "title";
     title.classList.add("titleEpisode");
-    title.innerHTML = `${episode.name} - S${formatNumber(
-      episode.season
+    title.innerHTML = `${episode.name} - S${formatNumber(episode.season
     )}E${formatNumber(episode.number)}`;
     let episodeImage = document.createElement("img");
     episodeImage.classList.add("imageEpisode");
     episodeImage.src = episode.image.medium;
+
+    displayNumber(allEpisodes.length, episodeList.length);
 
     episodeContainer.appendChild(title);
 
     episodeContainer.appendChild(episodeImage);
     episodeContainer.innerHTML += episode.summary;
     rootElem.appendChild(episodeContainer);
-  
+
+    
     
   });
   
